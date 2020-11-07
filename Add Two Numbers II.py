49b2c59250a911734fbb1b9e5f046c9f3e57ae9b
hsinhuibiga <hsinhuibiga@gmail.com>
2020-11-07 23:53:47 +0800

    Add files via upload

---
+++ Add Two Numbers II.py
@@ -0,0 +1,30 @@
+#Add Two Numbers II
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        num1 = ''
+        num2 = ''
+        while l1:
+            num1 += str(l1.val)
+            l1 = l1.next
+        while l2:
+            num2 += str(l2.val)
+            l2 = l2.next
+        add = str(int(num1) + int(num2))
+        head = ListNode(add[0])
+        answer = head
+        for i in range(1, len(add)):
+            node = ListNode(add[i])
+            head.next = node
+            head = head.next
+        return answer
